01e4fe523d422618ac617f5d0d393132074f8c65
Alex E Scott <aescott@outlook.com>
2017-05-16 16:52:59 -0700

    bootstrap current user

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -8,6 +8,7 @@ import {
 } from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -18,8 +19,8 @@ const App = () => (
     </header>
 
 
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
