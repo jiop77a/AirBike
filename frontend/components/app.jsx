940bf2998a0f3819491d477aaca95b9ee79c06d3
Alex E Scott <aescott@outlook.com>
2017-05-16 15:42:01 -0700

    add react router and updated entry point

---
+++ frontend/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link,
+  HashRouter
+} from 'react-router-dom';
+
+const App = () => (
+  <div>
+    <h1>Air Bike</h1>
+  </div>
+);
+
+export default App;
--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
-    window.store = configureStore();
-    ReactDOM.render(<h1>Welcome to AirBike</h1>, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={ store }/>, root);
 });
