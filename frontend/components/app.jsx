eddb6d586481f029143f8f368761e2a927c090d3
Alex E Scott <aescott@outlook.com>
2017-05-16 15:55:36 -0700

    add greeting components

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -6,10 +6,12 @@ import {
   Link,
   HashRouter
 } from 'react-router-dom';
+import GreetingContainer from './greeting/greeting_container';
 
 const App = () => (
   <div>
     <h1>Air Bike</h1>
+    <GreetingContainer />
   </div>
 );
 
