2cdfc0d35bc6be8556abe637178ded913b30389b
Alex E Scott <aescott@outlook.com>
2017-05-20 17:50:32 -0700

    fix image scaling problems

--- frontend/components/bike_detail/bike_detail.jsx
+++ frontend/components/bike_detail/bike_detail.jsx
@@ -15,9 +15,40 @@ class BikeDetail extends Component {
     const { bikeDetail } = this.props;
     return (
       <section className="bike-detail">
-        <figure>
+        <figure className="bike-detail-figure">
           <img src ={bikeDetail.picture_url} alt={bikeDetail.description} />
         </figure>
+        <section className="details-and-form">
+          <div className="detail-content">
+            <div className="bike-detail-label">
+              <span>{bikeDetail.city}</span>
+              <span>Type: {bikeDetail.variety}</span>
+            </div>
+            <div className="details-container">
+              <h2>About This Bike</h2>
+              <article className="bike-detail-description">
+                {bikeDetail.description}
+              </article>
+            </div>
+          </div>
+          <div className="booking-form">
+
+          </div>
+        </section>
+        <section className="reviews-and form">
+          <div className="review-list">
+            <h2>Reviews</h2>
+            <ul className="review-items">
+              <li>Lorem ipsum dolor sit amet, </li>
+              <li>consectetur adipisicing elit, </li>
+              <li>sed do eiusmod tempor incididunt ut </li>
+              <li>labore et dolore magna aliqua. Ut </li>
+            </ul>
+          </div>
+          <div className="review-form">
+
+          </div>
+        </section>
       </section>
 
 
