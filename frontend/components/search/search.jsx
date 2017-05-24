0513b25ac9e68991481981e4038b04e46d0d967a
Alex E Scott <aescott@outlook.com>
2017-05-23 17:50:37 -0700

    add bikes to search page

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BikeMap from './bike_map';
+import FeaturedBikeItem from '../featured_bikes/featured_bike_item';
 
 const Search = ({bikes, fetchAllBikes}) => (
   <div className="map-and-results">
@@ -9,7 +10,12 @@ const Search = ({bikes, fetchAllBikes}) => (
         bikes={bikes}
         />
     </div>
-
+    <div className="right-half">
+      <h2>Search Results</h2>
+      <ul className="featured-bikes-list">
+        {bikes.map(bike => <FeaturedBikeItem key = {bike.id} bike = {bike} />)}
+      </ul>
+    </div>
   </div>
 );
 
