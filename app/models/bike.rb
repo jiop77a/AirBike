1e246d5c6f4ce3df385ee57bed519e78c3d7f86f
Alex E Scott <aescott@outlook.com>
2017-05-24 14:48:47 -0700

    add bounds method to bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -11,6 +11,11 @@ class Bike < ActiveRecord::Base
 
   has_many :reviews
 
-  
+  def self.in_bounds(bounds)
+    self.where("lat < ?", bounds[:northEast][:lat])
+        .where("lat > ?", bounds[:southWest][:lat])
+        .where("lng > ?", bounds[:southWest][:lng])
+        .where("lng < ?", bounds[:northEast][:lng])
+  end
 
 end
