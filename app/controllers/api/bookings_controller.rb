e8e224d565852b215f8c32cac02b440049311ae2
Alex E Scott <aescott@outlook.com>
2017-05-25 15:37:11 -0700

    add bookings controller and views

--- app/controllers/api/bookings_controller.rb
+++ app/controllers/api/bookings_controller.rb
@@ -1,2 +1,34 @@
 class Api::BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.all.where("user_id = ?", params[:user_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      render :show
+    else
+      render json: @booking.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+
+    if @booking.destroy
+      render :show
+    else
+      render json: @booking.errors.full_messages, status: 422
+    end
+  end
+
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :bike_id, :user_id)
+  end
 end
