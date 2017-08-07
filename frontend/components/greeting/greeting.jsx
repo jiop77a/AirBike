187a05c5de67c6a855f7dc708f0ed055c37aaba3
Alex E Scott <aescott@outlook.com>
2017-08-07 11:51:58 -0700

    secure images on cloudinary

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -6,7 +6,7 @@ import BikeDetailContainer from '../bike_detail/bike_detail_container';
 const personalGreeting = (currentUser, logout) => (
   <div>
     <header>
-      <Link to="/"><img src="http://orbizadventure.com/wp-content/uploads/2016/05/Repair.jpg"></img></Link>
+      <Link to="/"><img src="https://res.cloudinary.com/dol1mm8bd/image/upload/v1502131798/Repair_luqkgb.jpg"></img></Link>
       <hgroup className="header-group">
         <h2 className="header-name">Hi, {currentUser.username}!</h2>
         <Link className = "bookings-link" to={`/bookings/${currentUser.id}`}>Your Bookings</Link>
@@ -19,7 +19,7 @@ const personalGreeting = (currentUser, logout) => (
 const impersonalGreeting = () => (
   <div>
     <header>
-      <Link to="/"><img src="http://orbizadventure.com/wp-content/uploads/2016/05/Repair.jpg"></img></Link>
+      <Link to="/"><img src="https://res.cloudinary.com/dol1mm8bd/image/upload/v1502131798/Repair_luqkgb.jpg"></img></Link>
       <hgroup className="header-group">
         <Link to="/signup" className="header-button">Sign Up</Link>
         <Link to="/login" className="header-button">Log In</Link>
