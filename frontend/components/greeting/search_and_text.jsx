71a2cd46cd93ef86e109b4625acd92367531585f
Alex E Scott <aescott@outlook.com>
2017-08-10 17:04:47 -0700

    styling nav-bar and promo-text

--- frontend/components/greeting/search_and_text.jsx
+++ frontend/components/greeting/search_and_text.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 
 export const SearchAndText = () => (
   <section className="search-and-text">
-    <div className='promo-text'>
-      <p className='welcome-line-1'>Welcome to <strong>AirBike!</strong></p>
-      <p className='welcome-line-2'>The site that connects <strong>home mechanics</strong> to <strong>visiting enthusiasts</strong></p>
+    <div className="promo-text-container">
+      <div className='promo-text'>
+        <p className='welcome-line-1'>AirBike</p>
+        <p className='welcome-line-2'>Connecting home mechanics to visiting enthusiasts.</p>
+      </div>
     </div>
     <div className='search-bar'>
       <Link to="/search" className="promo-button">Find a bike today!</Link>
