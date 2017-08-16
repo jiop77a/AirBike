13f4bf610eaa54c311c59d8e6b4b6797f8b2e49c
Alex E Scott <aescott@outlook.com>
2017-08-15 17:33:37 -0700

    provisional bike label styling

--- frontend/components/featured_bikes/featured_bike_item.jsx
+++ frontend/components/featured_bikes/featured_bike_item.jsx
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 const FeaturedBikeItem = ({ bike }) => (
   <li className="featured-bike-item">
     <Link to={`/bikes/${bike.id}`}>
-      <div className="featured-bike-label">
-        <span>${bike.cost}</span>
-        <span><strong>{bike.city}</strong></span>
-      </div>
       <img src={bike.picture_url} alt={bike.description} />
+      <div className="featured-bike-label">
+        <div className="featured-cost-description">
+          <p>${bike.cost}</p><p> {bike.description}</p>
+        </div>
+        <div className="featured-city">
+          <span><strong>{bike.city}</strong></span>
+        </div>
+      </div>
     </Link>
   </li>
 );
