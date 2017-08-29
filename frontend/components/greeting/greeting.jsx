f912ac4b3150634da12cfdd9d9279f05df2f8c49
Alex E Scott <aescott@outlook.com>
2017-08-29 11:31:58 -0700

    pre-branch commit

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { Link, Redirect, Route } from 'react-router-dom';
 import BikeDetailContainer from '../bike_detail/bike_detail_container';
 
+const iconURL =
+ "https://res.cloudinary.com/dol1mm8bd/image/upload/v1502223298/Repair_luqkgb_plrcpv.png"
+
 const personalGreeting = (currentUser, logout) => (
   <div>
     <header>
-      <Link to="/"><img src="https://res.cloudinary.com/dol1mm8bd/image/upload/v1502223298/Repair_luqkgb_plrcpv.png"></img></Link>
+      <Link to="/">
+        <img src={iconURL}></img>
+      </Link>
       <hgroup className="header-group">
         <h2 className="header-name">Hi, {currentUser.username}!</h2>
         <Link className = "nav-link" to={`/bookings/${currentUser.id}`}>Your Bookings</Link>
@@ -19,7 +24,9 @@ const personalGreeting = (currentUser, logout) => (
 const impersonalGreeting = () => (
   <div>
     <header>
-      <Link to="/"><img src="https://res.cloudinary.com/dol1mm8bd/image/upload/v1502223298/Repair_luqkgb_plrcpv.png"></img></Link>
+      <Link to="/">
+        <img src={iconURL}></img>
+      </Link>
       <hgroup className="header-group">
         <Link to="/signup" className="nav-link">Sign Up</Link>
         <Link to="/login" className="nav-link">Log In</Link>
