c982abc0e8ea9db02b5306d90e0d5ccc220afcc0
Alex E Scott <aescott@outlook.com>
2018-04-04 20:23:39 -0700

    postpone test file

--- spec/first_spec.rb
+++ spec/first_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'booking'
-
-describe Booking do
-
-  subject(:booking_one) {Booking.new}
-  describe '#start_must_be_in_the_future' do
-    it 'throws an error if start date has past'
-  end
-end
+# require_relative 'booking'
+#
+# describe Booking do
+#
+#   subject(:booking_one) {Booking.new}
+#   describe '#start_must_be_in_the_future' do
+#     it 'throws an error if start date has past'
+#   end
+# end
